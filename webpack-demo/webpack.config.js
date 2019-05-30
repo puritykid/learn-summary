84ee3644aa3c98279ecbac86d49860ff65830832
puritykid <18310507278@163.com>
2019-05-30 16:13:05 +0800

    添加webpac打包以及本地服务

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+	devtool: 'eval-source-map', // 只在开发时使用
 	entry: __dirname + '/app/main.js', //唯一入口文件
 	output: {
 		path: __dirname + '/public', // 打包后文件存放路径
 		filename: 'bundle.js' // 打包后输出文件的文件名
+	},
+
+	devServer:{
+		contentBase: './public', // 本地服务器加载页面所在目录
+		historyApiFallback: true, // 不跳转
+		inline: true // 实施刷新
 	}
 };
